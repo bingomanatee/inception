d109d37ae9d04a6a598616ca8b30f3a8763542b6
Dave Edelhart <dave@wonderlandlabs.com>
2019-03-10 21:08:46 -0700

    working with signal processing

--- src/Collection.test.js
+++ src/Collection.test.js
@@ -29,15 +29,69 @@ tap.test('Collection', (suite) => {
       testGetExists.same(daffySignal.action, {do: 'get', where: {id: 2}});
 
       const results = [];
-      daffySignal.subscribe(({data}) => {
+
+      daffySignal.subscribe((message) => {
+        const {data, error} = message;
         if (data) {
           results.push(data.map(d => d.toJSON()));
         }
+        else if (error) {
+          results.push({error: error.message});
+        }
       });
+
       testGetExists.same(results, []);
       daffySignal.perform();
       testGetExists.same(results, [
         [{fields: {id: 2, name: 'Daffy'}, collection: 'ducks'}]
+      ]);
+
+      testGetExists.end();
+    });
+
+    testGet.test('multiple signals -- (record that exists)', (testGetExists) => {
+      beforeEach();
+
+      let daffySignal = ducks.get(2);
+      let donaldSignal = ducks.get(3);
+
+      testGetExists.same(daffySignal.action, {do: 'get', where: {id: 2}});
+
+      const donaldResults = [];
+
+      donaldSignal.subscribe((message) => {
+        const {data, error} = message;
+        if (data) {
+          donaldResults.push(data.map(d => d.toJSON()));
+        }
+        else if (error) {
+          donaldResults.push({error: error.message});
+        }
+      });
+
+      const daffyResults = [];
+
+      daffySignal.subscribe((message) => {
+        const {data, error} = message;
+        if (data) {
+          daffyResults.push(data.map(d => d.toJSON()));
+        }
+        else if (error) {
+          daffyResults.push({error: error.message});
+        }
+      });
+
+
+
+      testGetExists.same(daffyResults, []);
+      testGetExists.same(donaldResults, []);
+      daffySignal.perform();
+      donaldSignal.perform();
+      testGetExists.same(daffyResults, [
+        [{fields: {id: 2, name: 'Daffy'}, collection: 'ducks'}]
+      ]);
+      testGetExists.same(donaldResults, [
+        [{fields: {id: 3, name: 'Donald', note: 'Disney\'s duck'}, collection: 'ducks'}]
       ]);
 
       testGetExists.end();
@@ -52,9 +106,9 @@ tap.test('Collection', (suite) => {
 
       const results = [];
       daffySignal.subscribe((message) => {
-        const {error, data} = message;
+        const {data, error} = message;
         if (data) {
-          results.push(data.toJSON());
+          results.push(data.map(d => d.toJSON()));
         }
         else if (error) {
           results.push({error: error.message});
